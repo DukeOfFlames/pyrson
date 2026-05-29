cbd3fc15bfec7daf738e44069ea918e1f37d253a
DukeOfFlames <110727238+DukeOfFlames@users.noreply.github.com>
2026-05-29 17:00:50 +0200

    Support Linux in get_bin.py for binary selection

--- src/rson/get_bin.py
+++ src/rson/get_bin.py
@@ -8,6 +8,6 @@ def __get_bin_path__() -> Path:
     # Choose file based on OS.
     if sys.platform == "win32":
         return here / "binaries" / "windows" / "rson.exe"
-    elif sys.platform == "darwin":
+    elif sys.platform in ["darwin", "linux"]:
         return here / "binaries" / "linux" / "rson"
     else: raise Exception(f"{sys.platform} is not supported yet.")
